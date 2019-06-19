4bb3147d332fcc470644babdcde2c4b4f539cc72
vipulgarg192 <vipulgarg192@gmail.com>
2019-06-19 18:09:08 -0400

    This is initial commit

---
+++ SwiftHappyLife/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHappyLife
+//
+//  Created by MacStudent on 2019-06-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
